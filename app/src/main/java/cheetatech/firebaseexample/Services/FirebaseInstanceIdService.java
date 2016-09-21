1099cb6e2bd2522b4b415ce303c046e360f2febb
coderkan <erkanguzeler@gmail.com>
2016-09-21 21:34:39 +0300

    added firebase push notification services

---
+++ app/src/main/java/cheetatech/firebaseexample/Services/FirebaseInstanceIdService.java
@@ -0,0 +1,18 @@
+package cheetatech.firebaseexample.Services;
+
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class FirebaseInstanceIdService extends com.google.firebase.iid.FirebaseInstanceIdService {
+    private static final String REG_TOKEN = "REG_TOKEN";
+
+    @Override
+    public void onTokenRefresh()
+    {
+        String recent_token = FirebaseInstanceId.getInstance().getToken();
+        Log.d(REG_TOKEN,recent_token);
+    }
+
+}
---
+++ app/src/main/java/cheetatech/firebaseexample/Services/FirebaseMessageServices.java
@@ -0,0 +1,35 @@
+package cheetatech.firebaseexample.Services;
+
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import cheetatech.firebaseexample.MainActivity;
+import cheetatech.firebaseexample.R;
+
+public class FirebaseMessageServices extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
+        notificationBuilder.setContentTitle("CheetaTech NOTIFICATION");
+        notificationBuilder.setContentText(remoteMessage.getNotification().getBody());
+        notificationBuilder.setAutoCancel(true);
+        notificationBuilder.setSmallIcon(R.mipmap.ic_launcher);
+        notificationBuilder.setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0,notificationBuilder.build());
+    }
+
+
+}
